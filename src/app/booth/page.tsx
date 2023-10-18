0a4ac37525d98b06fa089033457fba971a4c40eb
hakyung <diveindeep.dev@gmail.com>
2023-10-18 19:06:20 +0900

    Implement save image with dom-to-image and file-saver

--- src/app/booth/page.tsx
+++ src/app/booth/page.tsx
@@ -2,27 +2,31 @@
 
 import { useSearchParams } from 'next/navigation';
 import Camera from '../_components/camera';
-import { MutableRefObject, useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import Previews from '../_components/previews';
 import Mockup from '../_components/mockup';
 import Palette from '../_components/palette';
 import Frame from '../_components/frame';
 import { frameOrders } from '@/config/frame';
+import domtoimage from 'dom-to-image';
+import { saveAs } from 'file-saver';
+import today from '@/utils';
 
 export default function Booth() {
   const searchParams = useSearchParams();
   const frameType = searchParams.get('frame') || 'basic';
   const [imgSrcs, setImgSrcs] = useState<string[]>([]);
   const [isNext, setIsNext] = useState<boolean>(false);
-  const [pickedColor, setPickedColor] = useState<string>(`000000`);
+  const [pickedColor, setPickedColor] = useState<string>('slate-950');
   const [error, setError] = useState<string>('');
-  const [currentOrder, setCurrentOrder] = useState<Orders>('first');
+  const [currentOrder, setCurrentOrder] = useState<Orders | ''>('first');
   const [selected, setSelected] = useState<Cuts>({
     first: '',
     second: '',
     third: '',
     fourth: '',
   });
+  const savedRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (imgSrcs.length === 8) {
@@ -46,13 +50,43 @@ export default function Booth() {
   };
 
   const selectPhoto = (imgSrc: string) => {
-    const current = frameOrders.indexOf(currentOrder);
+    const current = currentOrder !== '' ? frameOrders.indexOf(currentOrder) : 0;
     setSelected({ ...selected, [currentOrder]: imgSrc });
     setCurrentOrder(frameOrders[(current + 1) % 4]);
   };
 
+  const reset = () => {
+    setCurrentOrder('first');
+    setPickedColor('slate-950');
+    setSelected({
+      first: '',
+      second: '',
+      third: '',
+      fourth: '',
+    });
+  };
+
+  const save = () => {
+    setCurrentOrder('');
+    const savedImg = savedRef.current;
+    if (savedImg) {
+      const option = {
+        width: savedImg.clientWidth * 2,
+        height: savedImg.clientHeight * 2,
+        style: {
+          transform: 'scale(2)',
+          'transform-origin': '0% 0%',
+        },
+      };
+      (async () => {
+        const generate = await domtoimage.toBlob(savedImg, option);
+        saveAs(generate, `${today('file')}-4cuts.png`);
+      })();
+    }
+  };
+
   const capture = useCallback(
-    (ref: MutableRefObject<any>) => {
+    (ref: React.MutableRefObject<any>) => {
       if (imgSrcs.length >= 8) return;
 
       const imageSrc = ref.current.getScreenshot();
@@ -81,7 +115,7 @@ export default function Booth() {
         </div>
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2">
-          <div className="my-auto">
+          <div className="my-auto" ref={savedRef}>
             <Frame
               color={pickedColor}
               type={frameType}
@@ -102,16 +136,16 @@ export default function Booth() {
       )}
       <div className="flex lg:flex-col">
         <div
-          onClick={retake}
+          onClick={!isNext ? retake : reset}
           className="flex-center text-xl font-thin w-1/2 h-[100px] lg:w-full lg:h-1/2 bg-orange-50 hover:cursor-pointer hover:brightness-95"
         >
-          RETAKE
+          {!isNext ? 'RETAKE' : 'RESET'}
         </div>
         <div
-          onClick={goNext}
+          onClick={!isNext ? goNext : save}
           className="flex-center flex-col text-xl font-thin w-1/2 h-[100px] lg:w-full lg:h-1/2 bg-orange-50 hover:cursor-pointer hover:brightness-95"
         >
-          NEXT
+          {!isNext ? 'NEXT' : 'SAVE'}
         </div>
       </div>
     </div>
--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,15 @@
 const today = (type?: string) => {
   const now = new Date();
   const year = now.getFullYear();
+  const year2 = year - 2000;
   const month = now.getMonth() + 1;
   const monthForm = month < 10 ? `0${month}` : `${month}`;
   const date = now.getDate();
   const dateForm = date < 10 ? `0${date}` : `${date}`;
+
+  if (type === 'file') {
+    return `${year2}${monthForm}${dateForm}`;
+  }
 
   return `${year}. ${monthForm}. ${dateForm}`;
 };
